a99a7d567aa866a245e0ed3654287e02882ec8b3
Yago Ésquines <yago.fut@gmail.com>
2018-07-05 16:58:33 -0300

    Retirada de comandos não utilizados

--- ver_ip/ver_ipexterno.sh
+++ ver_ip/ver_ipexterno.sh
@@ -2,7 +2,6 @@
 #Autor: Yago Ésquines
 #Email: yago.fut@gmail.com
 #Ano: 2015
-#Versão: 1.1
 #Script para verificação e envio de IP Externo por email
 
 DIA=`date +%F`;
@@ -11,7 +10,6 @@ EMAIL="yago.fut@gmail.com";
 SWAPIP=`cat ./ver_ipexterno.txt`;
 
 #Verificar Conexão com a Internet
-#until [[ `ping -c 2 8.8.8.8 > /dev/null` ]]; do
 echo "#############################";
 echo "$DIA $HORA: PROCESSO COMEÇOU";
 ping -c2 8.8.8.8 > /dev/null;
@@ -26,7 +24,6 @@ if [[ $? -eq 0 ]]; then
 		#Verificando se IP Mudou
 			if [[ "$SWAPIP" != "$IPEXTERNO" ]]; then
 				echo "$DIA $HORA: Número do IP Externo: $IPEXTERNO";	
-				#SWAPIP=$IPEXTERNO;
 				#enviando IP por email
 				echo "Segue IP Externo de $DIA $HORA: $IPEXTERNO" | mutt -s 'JOKER - IP EXTERNO' $EMAIL;	
 				if [[ $? -eq 0 ]]; then
@@ -58,4 +55,3 @@ else
 	echo "$DIA $HORA: Nova Verificação em 30m.";
 	echo "#############################";
 fi
-#done
