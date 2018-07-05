e43a741c4bcd1a8aaaf490776a58f891fdd7bf18
Yago Ésquines <yago.fut@gmail.com>
2018-07-05 17:13:11 -0300

    Ajustes minimos

--- vh_apache/vhapache-full.sh
+++ vh_apache/vhapache-full.sh
@@ -6,7 +6,7 @@
 opcaohttps()
 	{
     	read -p "Deseja opção HTTPS? S/N: " OPHTTPS;
-    		if [ "$OPHTTPS" = 'S' -o "$OPHTTPS" = 's' ]; then {
+    		if [ "$OPHTTPS" = 'S' -o "$OPHTTPS" = 's' ]; then 
         	VOPHTTPS1="NameVirtualHost *:443";
     		VOPHTTPS2="<VirtualHost  *:443>
 			ServerAdmin ${ADMEMAIL}
@@ -34,62 +34,50 @@ opcaohttps()
 			</VirtualHost>";
         	OPHTTPS="HABILITADO";
         	opcaoredict
-    		}
-    		else {
-        		if [ "$OPHTTPS" = 'N' -o "$OPHTTPS" = 'n' ]; then {
+    		
+    		elif [ "$OPHTTPS" = 'N' -o "$OPHTTPS" = 'n' ]; then 
         		VOPHTTPS1="";
             	VOPHTTPS2="";
                 OPHTTPS="DESABILITADO";
                 OPREDIRECT="DESABILITADO";
-	        	}
-    			else {
-    				echo "[ERRO] OPÇAO INCORRETA";
-            		opcaohttps
-    				}
-        		fi }
-    		fi
+    		else 
+    			echo "[ERRO] OPÇAO INCORRETA";
+            	opcaohttps		
+        	fi 
 	}
 
 opcaosenha()
 	{
     read -p "Deseja opção Senha no Site? S/N: " OPSENHA;
-    		if [ "$OPSENHA" = 'S' -o "$OPSENHA" = 's' ]; then {
+    		if [ "$OPSENHA" = 'S' -o "$OPSENHA" = 's' ]; then 
     		VOPSENHA='AuthType Basic
 			AuthName "Acesso Restrito:"
 			AuthUserFile "/www/.htpasswd"
 			Require valid-user';
         	OPSENHA="HABILITADO";
-    		}
-    		else {
-        		if [ "$OPSENHA" = 'N' -o "$OPSENHA" = 'n' ]; then {
+    		
+    		elif [ "$OPSENHA" = 'N' -o "$OPSENHA" = 'n' ]; then 
         			VOPSENHA="";
                 	OPSENHA="DESABILITADO";
-        			}
-    				else {
-    					echo "[ERRO] OPÇAO INCORRETA";
-            			opcaosenha
-    					}
-        		fi }
+    		else 
+    				echo "[ERRO] OPÇAO INCORRETA";
+            		opcaosenha
     		fi
 	}
 
 opcaoredict()
 	{
     read -p "Deseja opção Redirecionamento (Forçar HTTPS)? S/N: " OPREDIRECT;
-	    	if [ "$OPREDIRECT" = 'S' -o "$OPREDIRECT" = 's' ]; then {
+	    	if [ "$OPREDIRECT" = 'S' -o "$OPREDIRECT" = 's' ]; then 
     		VREDIRECT="Redirect permanent / https://${DOMINIO}";
         	OPREDIRECT="HABILITADO";
-    		}
-    		else {
-        		if [ "$OPREDIRECT" = 'N' -o "$OPREDIRECT" = 'n' ]; then {
+    		
+    		elif [ "$OPREDIRECT" = 'N' -o "$OPREDIRECT" = 'n' ]; then 
         		VREDIRECT="";
                 OPREDIRECT="DESABILITADO";
-        		}
-    			else {
+    		else 
     			echo "[ERRO] OPÇAO INCORRETA";
             	opcaoredict
-    			}
-        		fi }
     		fi
 	}
 ################################################
@@ -100,30 +88,26 @@ read -p "Digite o nome do Site [Apenas Nome]: " NOMESITE;
 read -p "Digite o Dominio: " DOMINIO;
 read -p "Digite a Pasta Principal [$NOMESITE]: " PASTASITE;
 #Em caso de pasta em branco utilizar o padrão.
-	if [ "$PASTASITE" = '' ]; then {
+	if [ "$PASTASITE" = '' ]; then 
 		PASTASITE="/www/$NOMESITE/";
     	echo "[INFO] UTILIZADO PASTA PADRÃO - $PASTASITE";
-        }
      fi
 #Verifica se pasta existe
-	if [ -d "$PASTASITE" ]; then {
+	if [ -d "$PASTASITE" ]; then 
    	read -p "Digite o Email do Administrador: " ADMEMAIL;
 #Em caso de email em branco utilizar o padrão.
-		if [ "$ADMEMAIL" = '' ]; then {
+		if [ "$ADMEMAIL" = '' ]; then 
 			ADMEMAIL="flaptrap.developer@gmail.com";
 			echo "[INFO] UTILIZADO EMAIL PADRÃO - $ADMEMAIL";
-        	}
      	fi
 
 #OPCOES DE HTTPS, SENHA NO SITE e Redirecionamento
 opcaohttps
 opcaosenha
 
-echo " ";
-echo "[PROCESSO] CRIANDO VIRTUAL HOSTA... ";
-	if [ -f "/etc/apache2/sites-available/$NOMESITE" ]; then { #/etc/apache2/sites-available/$NOMESITE
+echo -e "\n[PROCESSO] CRIANDO VIRTUAL HOST... ";
+	if [ -f "/etc/apache2/sites-available/$NOMESITE" ]; then
 		echo "[ERRO] VIRTUAL HOST JA EXISTE";
-    }
     else
 cat << EOF > /etc/apache2/sites-available/$NOMESITE
 ${VOPHTTPS1}
@@ -187,14 +171,13 @@ echo "[OK] CRIADO VIRTUAL HOST COM SUCESSO!";
 echo "[PROCESSO] HABILITANDO SITE...!";
 echo "[PROCESSO] CONFIGURANDO PERMISSÕES";
 a2ensite $NOMESITE && chmod 751 /etc/apache2/sites-available/$NOMESITE && chown apache:apache /etc/apache2/sites-available/$NOMESITE
-		if [ $? -eq 0 ]; then {
+		if [ $? -eq 0 ]; then 
         echo "[OK] PERMISSÕES CONFIGURADAS!";
     	echo "[OK] SITE HABILITADO!";
     	echo "[PROCESSO] REINICIANDO APACHE...!";
     	service apache2 restart
-    		if [ $? -eq 0 ]; then {
-    			echo "[OK] APACHE REINICIADO!";
-            	echo "";
+    		if [ $? -eq 0 ]; then 
+    			echo -e "[OK] APACHE REINICIADO!\n";
                 #Colocar Informações sobre a Configuração
 				echo "[INFO] Site: http://${DOMINIO}";
 				echo "[INFO] Diretorio de Arquivos: ${PASTASITE}";
@@ -202,12 +185,12 @@ a2ensite $NOMESITE && chmod 751 /etc/apache2/sites-available/$NOMESITE && chown 
 				echo "[INFO] Opção de Redirecionamento: ${OPREDIRECT}";
 				echo -e "[INFO] Opção de Senha: ${OPSENHA}\n";
 				echo "[FIM] PROCESSO FINALIZADO COM SUCESSO!";
-			} else echo "[ERRO] FALHA AO REINICIAR APACHE!";
+			 else echo "[ERRO] FALHA AO REINICIAR APACHE!";
         	fi
-    	} else echo "[ERRO] FALHA AO HABILITAR SITE!";
+    	 else echo "[ERRO] FALHA AO HABILITAR SITE!";
     	fi
     fi
-} else
+ else
 	echo "[ERRO] DIRETÓRIO DO SITE NÃO ENCONTRADO";
     echo "Necessário criar o diretório Padrão do site";
 fi
